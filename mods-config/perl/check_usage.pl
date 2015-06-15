42fadee880fa232c0b19468b6727228c40fa252a
Jake He <zhex900@gmail.com>
2015-06-15 08:01:54 +0000

    add license

--- mods-config/perl/check_usage.pl
+++ mods-config/perl/check_usage.pl
@@ -1,3 +1,24 @@
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+#
+# Modified by: Jake He <zhex900@gmail.com>
+#
+# Referece: This script is from FreeRADIUS Beginner's Guide by Dirk van der Walt, 
+#           page 172-173
+#
+
 #! usr/bin/perl -w
 
 use strict;
