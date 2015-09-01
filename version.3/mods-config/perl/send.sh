aed75d97ad73c990b446e10b5fe36a27e2cc62c4
zhex900 <zhex900@users.noreply.github.com>
2015-09-01 12:34:01 +0800

    Update send.sh

--- version.3/mods-config/perl/send.sh
+++ version.3/mods-config/perl/send.sh
@@ -2,10 +2,18 @@
 
 MAILFILE="/tmp/mailgun.queue"
 
-if [ -s "$MAILFILE" ]
-then
-        echo "$MAILFILE is exists and is not empty.\n Run script..."
-        bash $MAILFILE
-        echo "Empty the script file."
-        > $MAILFILE
-fi
+while :
+do
+        echo "Check for scripts to run."
+        if [ -s "$MAILFILE" ]
+        then
+                echo "$MAILFILE is exists and is not empty.\n Run script..."
+                bash $MAILFILE
+                echo "Empty the script file."
+                > $MAILFILE
+        else
+                echo "Nothing to run."
+        fi
+        echo "Wait for 5 minutes until the next run."
+        sleep 5m
+done
