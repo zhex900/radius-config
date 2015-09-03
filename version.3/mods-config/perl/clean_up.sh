8794fd72346520e616431653852519f00a6b246d
zhex900 <zhex900@users.noreply.github.com>
2015-09-03 21:34:49 +0800

    Create clean_up.sh

---
+++ version.3/mods-config/perl/clean_up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# run cleanup.pl once a month
+while :
+do
+        # run clean up
+        echo "Run clean up at: " `date`
+        perl ./clean_up.pl
+
+        # schedule for the next run.
+        startTime=$(date +%s)
+        endTime=$(date -d "next month" +%s)
+        timeToWait=$(($endTime- $startTime))
+        days=$(($timeToWait/86400))
+        echo "Next clean up will in " $days " days"
+        sleep $timeToWait
+done
